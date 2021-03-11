fe90ece30c2e22238b8c6caeb1f26939f72a0854
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-03-11 19:30:36 -0300

    update validaNome

--- SitePerfil/js/index.js
+++ SitePerfil/js/index.js
@@ -10,9 +10,14 @@ function validaForms(){
 
 function validaNome(){
     let nome= document.querySelector("input#nome");
+    let verificaNome= document.querySelector("#verificaNome");
     if(nome.value.length <= 2){
         nome.style.backgroundColor= '#ffbfaa';
+        verificaNome.style.backgroundColor= '#ffbfaa';
+        verificaNome.innerHTML = "Nome Inválido"
     }else{
         nome.style.backgroundColor= '#F0FFF0';
+        verificaNome.style.backgroundColor= '#F0FFF0';
+        verificaNome.innerHTML = "Nome Válido"
     }
 }
